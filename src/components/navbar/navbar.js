2196c40a981b2e086130fadfd75890536196977f
SarTzortzis <p3170157@gmail.com>
2022-07-16 13:05:05 +0300

    2nd commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar() {
-  return <section className="navbar_main">Navbar</section>;
+  return <section className="navbar_main">Navbar test push</section>;
 }
 
 export default Navbar;
